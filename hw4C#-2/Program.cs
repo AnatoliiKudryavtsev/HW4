d1c59a28c2df6e7a17aa7fe8cb60cea6c7a11d72
unknown <Anatolii>
2022-12-15 14:22:56 +0300

    add folders

---
+++ hw4C#-2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{IntSum(Number)}");
+int IntSum(int Number)
+{
+    int sum=0;
+    for ( ;Number > 0; )
+    
+    {
+        sum += Number % 10;
+        Number /= 10;
+    }
+    return sum;
+}
---
+++ hw4C#-3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая задаёт массив 
+//из 8 элементов и выводит их на экран.
+
+int[] numbers = new int[8];
+
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0, 100);
+}
+Console.WriteLine($"[{String.Join(",", numbers)}]");
